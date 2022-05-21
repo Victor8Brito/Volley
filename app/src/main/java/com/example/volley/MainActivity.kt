0396ed049dcfcfca9d366bf8562cc0face22f405
Victor8Brito <victor8brito@gmail.com>
2022-05-21 18:35:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/volley/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.volley
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://www.google.com"
+
+        val stringRequest = StringRequest(Request.Method.GET,url,Response.Listener { respuesta ->
+            Log.d("petición", "${respuesta.substring(0,500)}")
+        },Response.ErrorListener {
+            Log.e("petición","Hubo un error")
+        })
+        queue.add(stringRequest)
+    }
+}
